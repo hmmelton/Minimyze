921b5eb44761857cdb7dc5f6bf3f6f010f708218
Harrison Melton <hmmelton@comcast.net>
2017-01-30 16:12:10 -0800

    removed ButterKnife from Splashscreen

--- app/src/main/java/com/sonora/android/SplashscreenActivity.java
+++ app/src/main/java/com/sonora/android/SplashscreenActivity.java
@@ -4,10 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -15,24 +11,11 @@ import butterknife.ButterKnife;
  */
 public class SplashscreenActivity extends AppCompatActivity {
 
-    @BindView(R.id.fullscreen_content) protected View mContentView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_splashscreen);
-        ButterKnife.bind(this);
-
-        // Note that some of these constants are new as of API 16 (Jelly Bean)
-        // and API 19 (KitKat). It is safe to use them, as they are inlined
-        // at compile-time and do nothing on earlier devices.
-        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
 
         // TODO: Remove this
         new Handler().postDelayed(() -> {
