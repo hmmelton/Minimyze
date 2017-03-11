9172d8397aed2b03eb63b021e77d3fb20752b827
Harrison Melton <hmmelton@comcast.net>
2017-03-11 10:36:33 -0800

    created ProfileFragment

---
+++ app/src/main/java/com/sonora/android/fragments/ProfileFragment.java
@@ -0,0 +1,10 @@
+package com.sonora.android.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by harrisonmelton on 3/11/17.
+ */
+
+public class ProfileFragment extends Fragment {
+}
