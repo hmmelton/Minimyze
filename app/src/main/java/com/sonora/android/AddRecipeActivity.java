ca4f9e5a3268467b1dc799b32126bfc1fa1fd24d
Harrison Melton <hmmelton@comcast.net>
2017-03-14 22:32:52 -0700

    added back button and title to action bar

--- app/src/main/java/com/sonora/android/AddRecipeActivity.java
+++ app/src/main/java/com/sonora/android/AddRecipeActivity.java
@@ -1,7 +1,11 @@
 package com.sonora.android;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+import butterknife.ButterKnife;
 
 public class AddRecipeActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class AddRecipeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_recipe);
+        ButterKnife.bind(this);
+
+        // Add back button and title to action bar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setTitle(R.string.title_add_recipe_activity);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            // Back button was pressed -- return to sending activity
+            setResult(RESULT_OK);
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
