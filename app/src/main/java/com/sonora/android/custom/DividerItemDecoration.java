073cd09f50adab5865269edc846928b6255f4596
Harrison Melton <hmmelton@comcast.net>
2017-03-27 16:42:03 -0700

    created simple TextView cell and line divider decoration

---
+++ app/src/main/java/com/sonora/android/custom/DividerItemDecoration.java
@@ -0,0 +1,10 @@
+package com.sonora.android.custom;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by harrison on 3/27/17.
+ */
+
+public class DividerItemDecoration extends RecyclerView.ItemDecoration {
+}
