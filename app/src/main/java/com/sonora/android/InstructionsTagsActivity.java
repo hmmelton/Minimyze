4f11caadf8974276adae939c2a07e9e819e7a748
Harrison Melton <hmmelton@comcast.net>
2017-04-03 21:21:40 -0700

    added new activity for adding instructions and tags to recipe

---
+++ app/src/main/java/com/sonora/android/InstructionsTagsActivity.java
@@ -0,0 +1,13 @@
+package com.sonora.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InstructionsTagsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instructions_tags);
+    }
+}
